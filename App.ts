7fb2ec1959e79004ddce4ffc501e1ba5ef088596
kapish-patel <kpatelkapish@gmail.com>
2023-04-28 19:55:54 -0700

    Added Comments

--- App.ts
+++ App.ts
@@ -1,17 +1,12 @@
-
-import { DbConnection } from "./DbConnection";
+//Imports
 import { RestaurantModel } from "./models/RestaurantModel";
 import express, { Express } from "express";
 import * as bodyParser from 'body-parser';
-
-
-
 
 //Class App which creates and configure the express application
 class App{
     public expressApp: express.Application;
     public Restaurants: RestaurantModel;
-
 
     //Constructor which runs the configuration on the express application and calls the routes function
     constructor()
