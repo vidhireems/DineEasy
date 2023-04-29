6bb030c358d2a640898630697c5f30b3ad7bd4d9
kapish-patel <kpatelkapish@gmail.com>
2023-04-28 19:57:08 -0700

    Added Comments

--- models/RestaurantModel.ts
+++ models/RestaurantModel.ts
@@ -1,19 +1,24 @@
+//Imports
 import Mongoose from 'mongoose';
 import { DbConnection } from "../DbConnection";
 import { IRestaurantModel } from '../interfaces/IRestaurantModel';
 
+//Mongoose connections and object
 let mongooseConnection = DbConnection.mongooseConnection;
 let mongooseObj = DbConnection.mongooseInstance;
 
+//Class for restaurant model
 class RestaurantModel {
     public schema:any;
     public model:any;
 
+    //constructor initilize the create schema and model
     public constructor() {
         this.createSchema();
         this.createModel();
     }
 
+    //function to create the schema for restaurants
     public createSchema(): void {
         this.schema = new Mongoose.Schema(
             {
@@ -35,10 +40,12 @@ class RestaurantModel {
         );
     }
 
+    //function to create model for the reataurant interface and schema
     public createModel(): void {
         this.model = mongooseConnection.model<IRestaurantModel>("restaurant", this.schema);
     }
 
+    // function for retriving all the restaurants(have to use promise after mongoose version 6)
     public async retrieveAllRestaurants(response: any): Promise<any> {
         try {
             const itemArray = await this.model.find().exec();
