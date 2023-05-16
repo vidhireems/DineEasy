e73fa31cb77f0b85ef6ad7c70fdf8590b6b80e85
kapish-patel <kpatelkapish@gmail.com>
2023-05-15 18:25:38 -0700

    clean up

--- App.ts
+++ App.ts
@@ -1,4 +1,4 @@
-	//Imports
+//Imports
 import { RestaurantModel } from "./models/RestaurantModel";
 import { MenuModel } from "./models/MenuModel";
 import { MenuItemsModel } from "./models/MenuItemsModel";
@@ -33,7 +33,7 @@ class App {
   private middleware(): void {
     this.expressApp.use(bodyParser.json());
     this.expressApp.use(bodyParser.urlencoded({ extended: true }));
-    this.expressApp.use(express.static('pages'));
+    this.expressApp.use(express.static("pages"));
     this.expressApp.use(cors());
   }
 
@@ -48,96 +48,107 @@ class App {
     });
 
     // Retrieve specific restaurant details
-    router.get('/restaurants/:resId', (req: any, res: any) => {
+    router.get("/restaurants/:resId", (req: any, res: any) => {
       let resId = req.params.resId;
-      console.log('Query single restaurant with id: ' + resId);
-      this.Restaurants.getRestaurantDetailsById(res, {resId: resId});
+      console.log("Query single restaurant with id: " + resId);
+      this.Restaurants.getRestaurantDetailsById(res, { resId: resId });
     });
-  
 
     // Routing post order requests to save data
-    //Create restaurant 
-    router.post("/restaurants", (request:any, response:any) => {
+    //Create restaurant
+    router.post("/restaurants", (request: any, response: any) => {
       console.log("Creating restaurant");
       this.Restaurants.createRestaurant(request, response);
     });
 
     //Delete Restaurant
-    router.delete("/restaurants/:resId",(req:any,res:any) => {
+    router.delete("/restaurants/:resId", (req: any, res: any) => {
       this.MenuItems.deleteAllMenuItemsForRestaurant(req, res, () => {
         this.Menu.deleteAllMenuForRestaurant(req, res, () => {
-          this.Restaurants.deleteRestaurant(req,res)
+          this.Restaurants.deleteRestaurant(req, res);
         });
       });
     });
 
-    //Update restaurant 
-    router.put("/restaurants/:resId", (req:any, res:any) => {
-      this.Restaurants.updateRestaurant(req,res)
+    //Update restaurant
+    router.put("/restaurants/:resId", (req: any, res: any) => {
+      this.Restaurants.updateRestaurant(req, res);
     });
-  
+
     //Retrieve Menu
-    router.get("/restaurants/:resId/menu", (req:any, res:any) => {
+    router.get("/restaurants/:resId/menu", (req: any, res: any) => {
       var resId = req.params.resId;
       console.log("Query single menu with restid: " + resId);
       this.Menu.retrieveMenu(res, { resId: resId });
     });
 
     //Create menu
-    router.post("/restaurants/:resId/menu", (req:any, res:any) => {
+    router.post("/restaurants/:resId/menu", (req: any, res: any) => {
       this.Menu.createMenu(req, res);
     });
 
     //Delete menu
-    router.delete("/restaurants/:resId/menu/:menuId", (req:any, res:any) => {
+    router.delete("/restaurants/:resId/menu/:menuId", (req: any, res: any) => {
       this.MenuItems.deleteAllMenuItems(req, res, () => {
         this.Menu.deleteMenu(req, res);
       });
     });
 
     //Retrieve Menu Items
-    router.get("/restaurants/:resId/menu/:menuId/items", (req:any, res:any) => {
-      var resId = req.params.resId;
-      var menuId = req.params.menuId;
-      console.log("Query single menu with restid: " + resId);
-      this.MenuItems.retrieveMenuItems(res, {
-      menuId: menuId,
-      resId: resId,
-      });
-    });
+    router.get(
+      "/restaurants/:resId/menu/:menuId/items",
+      (req: any, res: any) => {
+        var resId = req.params.resId;
+        var menuId = req.params.menuId;
+        console.log("Query single menu with restid: " + resId);
+        this.MenuItems.retrieveMenuItems(res, {
+          menuId: menuId,
+          resId: resId,
+        });
+      }
+    );
 
     // post- createorder
 
     // Create menu Items
-    router.post("/restaurants/:resId/menu/:menuId/items", (req:any, res:any) => {
-      this.MenuItems.createMenuItems(req, res);
-    });
+    router.post(
+      "/restaurants/:resId/menu/:menuId/items",
+      (req: any, res: any) => {
+        this.MenuItems.createMenuItems(req, res);
+      }
+    );
 
     // Delete menu Items
-    router.delete("/restaurants/:resId/menu/:menuId/items", (req:any, res:any) => {
-      this.MenuItems.deleteMenuItems(req, res);
-    });
+    router.delete(
+      "/restaurants/:resId/menu/:menuId/items",
+      (req: any, res: any) => {
+        this.MenuItems.deleteMenuItems(req, res);
+      }
+    );
 
     // update menu Items
-    router.patch("/restaurants/:resId/menu/:menuId/items", (req:any, res:any) => {
-      this.MenuItems.updateMenuItems(req, res);
-    });
+    router.patch(
+      "/restaurants/:resId/menu/:menuId/items",
+      (req: any, res: any) => {
+        this.MenuItems.updateMenuItems(req, res);
+      }
+    );
 
     // post order
-    router.post("/orders", (request:any, response:any) => {
+    router.post("/orders", (request: any, response: any) => {
       this.Orders.createOrder(request, response);
     });
 
     // post- create customer
-    router.post("/addcustomer", (request:any, response:any) => {
+    router.post("/addcustomer", (request: any, response: any) => {
       console.log("Adding New Customer:...");
       this.Customer.createCustomer(request, response);
     });
 
     //post- customer update
-    router.post("/updatecustomer/:customerId", (req:any, res:any) =>{
+    router.post("/updatecustomer/:customerId", (req: any, res: any) => {
       console.log("Update Customer:...");
-      this.Customer.updateCustomer(req,res);
+      this.Customer.updateCustomer(req, res);
     });
 
     this.expressApp.use("/", router);
