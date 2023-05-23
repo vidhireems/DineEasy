a2bf85e346d6038551cec47b81486ebb758df042
Vidhi Rathod <vidhirathod@Vidhis-MacBook-Pro.local>
2023-05-23 03:56:48 -0700

    Updating site updates for localhost

--- AppServer.ts
+++ AppServer.ts
@@ -1,5 +1,16 @@
 //Imports
 import { App } from './App';
+import { DbConnection } from './DbConnection';
+
+
+// Handle localhost scenario
+const args = process.argv.slice(2);
+const envArgIndex = args.findIndex(arg => arg === '--env');
+if(envArgIndex !== -1 || args[envArgIndex + 1] === 'localhost')
+{
+    // Set the environment to indicate the use of the local database
+    DbConnection.setEnvironment('localhost');
+}
 
 //Initialize the server and listen on port 8080
 let server:any = new App().expressApp;
