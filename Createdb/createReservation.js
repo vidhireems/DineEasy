37f22e7e413348746a347948498f7787c486733b
Vidhi Rathod <vidhirathod@Vidhis-MacBook-Pro.local>
2023-05-23 02:13:56 -0700

    Updated DB scripts for cloud deployment

--- Createdb/createReservation.js
+++ Createdb/createReservation.js
@@ -1,8 +1,5 @@
-// Connect to mongo
-const conn = new Mongo();
-
 // Connect to dineEasy DB
-db = conn.getDB('dineEasy');
+db = db.getSiblingDB('dineEasy');
 
 // Create collection if not already present
 orderCollection = db.getCollection('reservation') || db.createCollection('reservation');
--- DbConnection.js
+++ DbConnection.js
@@ -8,13 +8,16 @@ exports.DbConnection = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 //DbConnection helps in connecting to Mongodb 
 class DbConnection {
-    constructor() {
-        DbConnection.connect(); //TODO: Consider removing this
-    }
     //function to connect to Mongo Db 
     static connect() {
         if (this.mongooseInstance)
             return this.mongooseInstance;
+        // Handle localhost scenario
+        const args = process.argv.slice(2);
+        const envArgIndex = args.findIndex(arg => arg === '--env');
+        if (envArgIndex !== -1 || args[envArgIndex + 1] === 'localhost') {
+            this.DB_CONNECTION_STRING = `mongodb://localhost:${this.PORT}`;
+        }
         this.mongooseConnection = mongoose_1.default.connection;
         this.mongooseConnection.on("open", () => {
             console.log("Connected to mongodb.");
@@ -24,7 +27,8 @@ class DbConnection {
     }
 }
 exports.DbConnection = DbConnection;
-DbConnection.DB_CONNECTION_STRING = 'mongodb://dbAdmin:test@127.0.0.1:27017/dineEasy?authSource=admin';
+DbConnection.DB_CONNECTION_STRING = 'mongodb+srv://dbAdmin:test@cluster0.lcc9vdm.mongodb.net/dineEasy';
+DbConnection.PORT = '27017';
 DbConnection.connect();
 // clear models cache
 Object.keys(DbConnection.mongooseConnection.models).forEach(modelName => {
